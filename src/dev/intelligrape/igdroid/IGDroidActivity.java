ac74672a5b06d4cfe4c5d7a2187e729a960e5b75
Hitesh Bhatia <hitesh@intelligrape.com>
2011-08-31 20:08:49 +0530

    added file structure

---
+++ src/dev/intelligrape/igdroid/IGDroidActivity.java
@@ -0,0 +1,13 @@
+package dev.intelligrape.igdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IGDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
