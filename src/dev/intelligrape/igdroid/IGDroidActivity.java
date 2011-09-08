e49c7112da74d684e9635bb4fbaa6b9cc1e19e09
Hitesh Bhatia <hitesh@intelligrape.com>
2011-09-09 04:02:57 +0530

    add gitignore file

--- src/dev/intelligrape/igdroid/IGDroidActivity.java
+++ src/dev/intelligrape/igdroid/IGDroidActivity.java
@@ -1,7 +1,9 @@
 package dev.intelligrape.igdroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IGDroidActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,9 @@ public class IGDroidActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void enterButtonClicked(View v) {
+		Intent intent = new Intent(this, ContactInfoActivity.class);		
+		startActivity(intent);
+	}
 }
